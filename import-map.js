9dbb33c1c7d044ea15873bc3828043f74f431164
Pedro Sousa <sketchpunk@gmail.com>
2022-04-15 13:32:42 -0400

    Updates

--- import-map.js
+++ import-map.js
@@ -1,11 +1,10 @@
 // in the future can prob do : <script type="importmap" src="/import-map.json"></script>
 document.body.appendChild(Object.assign(document.createElement('script'), {
-	type		: 'importmap',
-	innerHTML	: `
-		{"imports":{
-			"three"			    : "/node_modules/three/build/three.module.js",
-			"OrbitControls"	    : "/node_modules/three/examples/jsm/controls/OrbitControls.js"
-		}}
-	`
-}));
-
+type		: 'importmap',
+innerHTML	: `
+    {"imports":{
+        "three"             : "/node_modules/three/build/three.module.js",
+        "OrbitControls"	    : "/node_modules/three/examples/jsm/controls/OrbitControls.js",
+        "webxr/"            : "/node_modules/three/examples/jsm/webxr/"
+    }}
+`}));
